62e6d55a3a0e92ce8482e8266f0d310ba122cc38
DimaAdmin <ryba-mech@yandex.ru>
2018-04-13 15:17:56 +0300

    10 - write component product-row .ts

--- src/app/product-row/product-row.component.ts
+++ src/app/product-row/product-row.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  HostBinding
+} from '@angular/core';
+import { Product } from '../product.model';
 
+/**
+ * @ProductRow: A component for the view of single Product
+ */
 @Component({
   selector: 'product-row',
   templateUrl: './product-row.component.html',
-  styleUrls: ['./product-row.component.css']
 })
-export class ProductRowComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ProductRowComponent {
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'item';
 }
