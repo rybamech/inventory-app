f2b087340d0e215428c4e51fc621c3ab55dd9f10
DimaAdmin <ryba-mech@yandex.ru>
2018-04-13 10:07:43 +0300

    07 - final create products-list component and template

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+import { Product } from '../product.model';
 
+/**
+ * @ProductsList: A component for rendering all ProductRows and
+ * storing the currently selected Product
+ */
 @Component({
-  selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  selector: 'products-list',
+  templateUrl: './products-list.component.html'
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent {
+  /**
+   * @input productList - the Product[] passed to us
+   */
+  @Input() productList: Product[];
 
-  constructor() { }
+  /**
+   * @output onProductSelected - outputs the current
+   *          Product whenever a new Product is selected
+   */
+  @Output() onProductSelected: EventEmitter<Product>;
 
-  ngOnInit() {
+  /**
+   * @property currentProduct - local state containing
+   *             the currently selected `Product`
+   */
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
   }
 
 }
